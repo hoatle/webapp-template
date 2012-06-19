9242a1b625b72f553234184f34c932de65ef28f6
Hoat Le <hoatlevan@gmail.com>
2012-06-19 23:26:37 +0700

    Issue #6 | application loading bar with basic stuff: loading with dot indicator and displaying what module name at what url

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -14,71 +14,111 @@
  * limitations under the License.
  */
 
-// Require.js allows us to configure shortcut alias
-require.config({
-  paths: {
-    'text': 'lib/require/plugins/text-2.0.0',
-    'handlebars': 'lib/handlebars/handlebars-1.0.0.beta.6',
-    'hbs': 'lib/require/plugins/hbs-0.2.1',
-    'json2': 'lib/json/json2',
-    'jquery': 'lib/jquery/jquery-1.7.1',
-    'underscore': 'lib/underscore/underscore-1.3.3',
-    'backbone': 'lib/backbone/backbone-0.9.2',
-    'bootstrap': 'lib/bootstrap/bootstrap-2.0.4',
-    'jquery.log': 'lib/jquery/plugins/jquery.log-1.0.0'
-  },
+(function() {
+  'use strict';
 
-  shim: {
+  var root = this;
 
-    json2: {
-      exports: 'JSON'
+  // Require.js allows us to configure shortcut alias
+  require.config({
+    paths: {
+      'domReady': 'lib/require/plugins/domReady-2.0.0',
+      'text': 'lib/require/plugins/text-2.0.0',
+      'handlebars': 'lib/handlebars/handlebars-1.0.0.beta.6',
+      'hbs': 'lib/require/plugins/hbs-0.2.1',
+      'json2': 'lib/json/json2',
+      'jquery': 'lib/jquery/jquery-1.7.1',
+      'underscore': 'lib/underscore/underscore-1.3.3',
+      'backbone': 'lib/backbone/backbone-0.9.2',
+      'bootstrap': 'lib/bootstrap/bootstrap-2.0.4',
+      'jquery.log': 'lib/jquery/plugins/jquery.log-1.0.0'
     },
 
-    underscore: {
-      exports: '_'
-    },
+    shim: {
 
-    backbone: {
-      deps:
-        [
-          'underscore',
-          'jquery'
-        ],
-      exports: 'Backbone'
-    },
+      json2: {
+        exports: 'JSON'
+      },
 
-    handlebars: {
-      exports: 'Handlebars'
-    },
+      underscore: {
+        exports: '_'
+      },
 
-    //jquery plugins
-    'bootstrap': ['jquery'],
+      backbone: {
+        deps:
+          [
+            'underscore',
+            'jquery'
+          ],
+        exports: 'Backbone'
+      },
 
-    'jquery.log': {
-      deps: ['jquery'],
-      exports: 'jQuery.fn.log'
+      handlebars: {
+        exports: 'Handlebars'
+      },
+
+      //jquery plugins
+      'bootstrap': ['jquery'],
+
+      'jquery.log': {
+        deps: ['jquery'],
+        exports: 'jQuery.fn.log'
+      }
     }
-  }
 
-});
+  });
+
+  //this requires dom ready to update on ui, so this function expression
+  //will be implemented later when domReady.
+  var updateModuleProgress = function(context, map, depMaps) {
+    //when dom is not ready, do something more useful?
+    if (console.log) {
+      console.log('loading: ' + map.name + ' at ' + map.url);
+    }
+  };
+
+
+  require.onResourceLoad = function(context, map, depMaps) {
+    updateModuleProgress(context, map, depMaps);
+  };
+
+
+
+  require(['domReady'], function(domReady) {
+    domReady(function() {
+      //re-implement updateModuleProgress here for domReady
+      updateModuleProgress = function(context, map, depMaps) {
+        var loadingStatusEl = document.getElementById('loading-status'),
+          loadingModuleNameEl = document.getElementById('loading-module-name');
+        var nextLoadingStatusContent = loadingStatusEl.innerHTML += '.'; //add one more dot character
+        loadingStatusEl.innerHTML = nextLoadingStatusContent;
+
+        var loadingModuleNameContent = map.name + ' at ' + map.url;
+
+        loadingModuleNameEl.innerHTML = loadingModuleNameContent;
+
+      };
+    });
+  });
 
 //load jquery plugins //TODO this is a bit ugly
-require(
-  [
-    'jquery',
-    'bootstrap',
-    'jquery.log'
-  ],
-  function($) {
+  require(
+    [
+      'jquery',
+      'bootstrap',
+      'jquery.log'
+    ],
+    function($) {
 
-    //boot the application
+      //boot the application
 
-    require(['app'], function(app) {
-      app.initialize();
-    });
-  }
-);
+      require(['app'], function(app) {
+        app.initialize();
+      });
+    }
+  );
 
+}).call(this);
 
 
 
