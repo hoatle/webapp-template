7d9e99ebe6db279974c3cf73d6f3a95d92a476be
Hoat Le <hoatlevan@gmail.com>
2012-07-21 02:35:08 +0700

    Issue #34 | Have LESS build for css: fix path error for bootstrap; force less recompile on each request with debug mode

--- server.js
+++ server.js
@@ -34,8 +34,12 @@ app.configure(function () {
 
 
 app.configure('dev', function () {
+  app.use(connectLess({
+    src: __dirname + '/webapp',
+    force: true,
+    debug: true
+  }));
   app.use(express.static(__dirname + '/webapp'));
-  app.use(connectLess({ src: __dirname + '/webapp/' }));
   app.use(express.static(__dirname + '/test'));
   app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
 });
