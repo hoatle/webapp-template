5427e49f374ba10a5f482282c7f8f24e599ab595
Hoat Le <hoatlevan@gmail.com>
2012-08-11 20:17:48 +0700

    Remove CDN link of jquery as currently, can not make it work

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -32,10 +32,7 @@
       'text': 'lib/require/plugins/text-2.0.1',
       'handlebars': 'lib/handlebars/handlebars-1.0.0.beta.6',
       'json2': 'lib/json/json2',
-      'jquery': [
-        '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min',
-        'lib/jquery/jquery-1.7.1'
-      ],
+      'jquery': 'lib/jquery/jquery-1.7.1',
       'underscore': 'lib/underscore/underscore-1.3.3',
       'backbone': 'lib/backbone/backbone-0.9.2',
       'Backbone.ModelBinder': 'lib/backbone/plugins/Backbone.ModelBinder-0.1.5',
